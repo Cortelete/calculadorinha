6dab6626a2310d67fefbede3e2d441a79f8f6a48
Cortelete <d.cortelete@gmail.com>
2024-08-02 16:50:02 -0300

    Correção mensagens

--- calculadorinha.js
+++ calculadorinha.js
@@ -24,11 +24,11 @@ function calcular(numero1, numero2, operacao) {
         case '3':
             return numero1 * numero2;
         case '4':
-            return numero2 !== 0 ? numero1 / numero2 : "Erro: Divisão por zero";
+            return numero2 !== 0 ? numero1 / numero2 : "Erro: Houve uma tentativa de divisão por zero";
         case '5':
             return (numero1 * numero2) / 100;
         default:
-            return "A operação é inválida.";
+            return "A operação realizada é inválida.";
     }
 }
 
@@ -38,14 +38,14 @@ obterEntrada("Insira o primeiro número: ", (entrada1) => {
 
     obterEntrada("Insira o segundo número: ", (entrada2) => {
         const numero2 = validarNumero(entrada2);
-        if (numero2 === null) return console.log("Entrada inválida. Por favor, reinicie e insira um número válido.");
+        if (numero2 === null) return console.log("Esta entrada foi inválida. Por favor, reinicie inserindo números válidos.");
 
-        obterEntrada("Selecione a operação desejada:\n1 - Adição (+)\n2 - Subtração (-)\n3 - Multiplicação (*)\n4 - Divisão (/)\n5 - Porcentagem (%)\n", (entradaOperacao) => {
+        obterEntrada("Selecione a operação:\n1 - Adição (+)\n2 - Subtração (-)\n3 - Multiplicação (*)\n4 - Divisão (/)\n5 - Porcentagem (%)\n", (entradaOperacao) => {
             const operacao = validarOperacao(entradaOperacao);
-            if (operacao === null) return console.log("Opção inválida. Por favor, reinicie e selecione uma operação válida.");
+            if (operacao === null) return console.log("Esta opção é inválida. Por favor, reinicie e selecione uma operação válida.");
 
             const resultado = calcular(numero1, numero2, operacao);
-            console.log(`O resultado da operação é: ${resultado}. Obrigado por utilizar a calculadorinha! :D`);
+            console.log(`O resultado da sua operação é: ${resultado}. Obrigado por utilizar a calculadorinha! :D`);
             process.stdin.pause();
         });
     });
